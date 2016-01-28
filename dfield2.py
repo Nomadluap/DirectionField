3f130f9df3d831a61734d90d2fdd7d2543f1905f
Paul Belanger <paulirino@gmail.com>
2016-01-27 22:26:01 -0500

    Initial commit.

---
+++ dfield2.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+from __future__ import division, print_function
+import matplotlib.pyplot as plt
+import numpy as np
+from math import atan, pi, sqrt
+
+BOTTOM_EXTENT = -10
+TOP_EXTENT = 10
+SAMPLES = 30
+
+def dydx(y, x):
+    return -2 + x - y
+
+
+def main():
+    fig = plt.figure(num = 1)
+    ax = fig.add_subplot(111)
+
+    # create vector field
+    X,Y = np.meshgrid(np.linspace(BOTTOM_EXTENT, TOP_EXTENT, SAMPLES),
+                      np.linspace(BOTTOM_EXTENT, TOP_EXTENT, SAMPLES))
+    U = 1
+    V = dydx(X, Y)
+    #normalize the arrows
+    N = np.sqrt(U**2 + V**2)
+    U2, V2 = U/N, V/N
+    ax.quiver(X, Y, U2, V2)
+
+    #show the plot
+    plt.xlim([BOTTOM_EXTENT, TOP_EXTENT])
+    plt.ylim([BOTTOM_EXTENT, TOP_EXTENT])
+    plt.xlabel(r"$x$")
+    plt.ylabel(r"$y(x)$")
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
